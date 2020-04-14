4165710b7551bf0f50b55a989e4666a4c4600e07
brunocampeao <brunocampeao@gmail.com>
2020-04-13 22:01:13 -0300

    Update index.js

--- JogoDaMemoria/index.js
+++ JogoDaMemoria/index.js
@@ -1,3 +1,47 @@
+	function setup() {
+		document.querySelectorAll(".card-container .flip-card-inner").forEach(item => {
+			item.addEventListener("click", function() {
+				console.log(this);
+
+				if(this.classList.contains("fix-card")) {
+					return;
+				}
+				if(this.classList.contains("rotate-card")) {
+					console.log("A");
+					this.classList.remove("rotate-card");
+					return;
+				}
+				console.log("B");
+				
+				if((selected = document.querySelectorAll(".rotate-card")[0]) != undefined) {
+					console.log("C");
+					var firstName = selected.querySelector(".card-back").getAttribute("data-name");
+					var  secondName = this.querySelector(".card-back").getAttribute("data-name");
+					if(firstName !== secondName) {
+					console.log("D");
+					  this.classList.add("rotate-card");
+					  var self = this;
+					  window.setTimeout(() => {self.classList.remove("rotate-card");selected.classList.remove("rotate-card");}, 700);
+					} else {
+					  this.classList.add("fix-card");
+					  selected.classList.add("fix-card");
+					  this.classList.remove("rotate-card");
+					  selected.classList.remove("rotate-card");
+					}
+				}
+				else {
+					this.classList.add("rotate-card");
+				}
+				
+			  
+			});
+			
+		});
+
+	}
+
+
+
 function onLoad() {
     const dependencias = {
         tela: Tela, // a classe tela é global
@@ -5,6 +49,12 @@ function onLoad() {
     }
     // inicializa mos o jogo da memoria
     window.jogoDaMemoria = new JogoDaMemoria(dependencias)
-    jogoDaMemoria.inicializar()
+    jogoDaMemoria.inicializar();
+    
+      document.getElementById("conteudo").addEventListener("DOMContentLoaded", function(event) {
+        console.log("DOM completamente carregado e analisado");
+        setup();
+      });
+    
 }
 window.onload = onLoad
